e6bbc3484051e2fffedab422ddd1b899bf5f51d0
Victoria.T <hturay1@gmail.com>
2022-06-27 10:16:38 -0400

    added contact page

--- Js/index.js
+++ Js/index.js
@@ -1,7 +1,6 @@
 const boutiqueController = new BoutiqueController(0);
-function addItemCard(item){
-
-const itemHTML =`<div class="col-sm-3 my-5 pt-5">
+function addItemCard(item) {
+  const itemHTML = `<div class="col-sm-3 my-5 pt-5">
 <div class="card description">
   <a href="#"
     ><img
@@ -17,73 +16,88 @@ const itemHTML =`<div class="col-sm-3 my-5 pt-5">
     </div>
   </div>
 </div>
-</div>`
-const itemsContainer = document.getElementById("list-items");
-itemsContainer.innerHTML += itemHTML;
+</div>`;
+  const itemsContainer = document.getElementById("list-items");
+  itemsContainer.innerHTML += itemHTML;
 }
 
-addItemCard({'name':'Green Sparkle Dress',
-    'price': '$98.00',
-    'description':'',
-    'picture':'./Resources/images/greendress.jpg',  
-    });
+addItemCard({
+  name: "Green Sparkle Dress",
+  price: "$98.00",
+  description: "",
+  picture: "./Resources/images/greendress.jpg",
+});
 
-addItemCard({'name':'Patterned Cardigan',
-    'price': '$85.00',
-    'description':'',
-    'picture':'./Resources/images/cardigan.jpg',  
-    });
-addItemCard({'name':'Golden Dress',
-    'price': '$108.00',
-    'description':'',
-    'picture':'./Resources/images/golddress.jpg',  
-    });
-addItemCard({'name':'Magenta Statement Dress',
-    'price': '$120.00',
-    'description':'',
-    'picture':'./Resources/images/magentadress.jpg',  
-    });  
-addItemCard({'name':'Sheer Pink Dress',
-    'price': '$100.00',
-    'description':'',
-    'picture':'./Resources/images/pinksheerdress.jpg',  
-    });
-addItemCard({'name':'Purple Shoulder Dress',
-    'price': '$110.00',
-    'description':'',
-    'picture':'./Resources/images/purpledress.jpg',  
-    });
-addItemCard({'name':'Red Floral Dress',
-    'price': '$102.00',
-    'description':'',
-    'picture':'./Resources/images/redfloral.jpg',  
-    });    
-addItemCard({'name':'Sheer Pattern Dress',
-    'price': '$106.00',
-    'description':'',
-    'picture':'./Resources/images/sheerdress.jpg',  
-    });
-addItemCard({'name':'Red Silhouette Dress',
-    'price': '$120.00',
-    'description':'',
-    'picture':'./Resources/images/redsideflow.jpg',  
-    });   
-addItemCard({'name':'Flowy Dress Shirt',
-    'price': '$102.00',
-    'description':'',
-    'picture':'./Resources/images/Linendress.jpg',  
-    });   
-addItemCard({'name':'White Fur Coat',
-    'price': '$150.00',
-    'description':'',
-    'picture':'./Resources/images/whitefurcoat.jpg',  
-    });   
-addItemCard({'name':'Red Mini Dress',
-    'price': '$98.00',
-    'description':'',
-    'picture':'./Resources/images/redminidress.jpg',  
-    });
+addItemCard({
+  name: "Patterned Cardigan",
+  price: "$85.00",
+  description: "",
+  picture: "./Resources/images/cardigan.jpg",
+});
+addItemCard({
+  name: "Golden Dress",
+  price: "$108.00",
+  description: "",
+  picture: "./Resources/images/golddress.jpg",
+});
+addItemCard({
+  name: "Magenta Statement Dress",
+  price: "$120.00",
+  description: "",
+  picture: "./Resources/images/magentadress.jpg",
+});
+addItemCard({
+  name: "Sheer Pink Dress",
+  price: "$100.00",
+  description: "",
+  picture: "./Resources/images/pinksheerdress.jpg",
+});
+addItemCard({
+  name: "Purple Shoulder Dress",
+  price: "$110.00",
+  description: "",
+  picture: "./Resources/images/purpledress.jpg",
+});
+addItemCard({
+  name: "Red Floral Dress",
+  price: "$102.00",
+  description: "",
+  picture: "./Resources/images/redfloral.jpg",
+});
+addItemCard({
+  name: "Sheer Pattern Dress",
+  price: "$106.00",
+  description: "",
+  picture: "./Resources/images/sheerdress.jpg",
+});
+addItemCard({
+  name: "Red Silhouette Dress",
+  price: "$120.00",
+  description: "",
+  picture: "./Resources/images/redsideflow.jpg",
+});
+addItemCard({
+  name: "Flowy Dress Shirt",
+  price: "$102.00",
+  description: "",
+  picture: "./Resources/images/Linendress.jpg",
+});
+addItemCard({
+  name: "White Fur Coat",
+  price: "$150.00",
+  description: "",
+  picture: "./Resources/images/whitefurcoat.jpg",
+});
+addItemCard({
+  name: "Red Mini Dress",
+  price: "$98.00",
+  description: "",
+  picture: "./Resources/images/redminidress.jpg",
+});
 
-
-
-
+addItemCard({
+  name: "Polkadots Sheer Dress",
+  price: "$120.00",
+  description: "",
+  picture: "./Resources/images/Polkadots_sheer.jpg",
+});
